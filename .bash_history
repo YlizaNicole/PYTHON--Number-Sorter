eaa0507ce0310395a01f70bc80e33d1c1a3dfa87
yli <salazarylizanicole@gmail.com>
2021-11-25 23:29:53 +0800

    i think im almost done

---
+++ .bash_history
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt dist-upgrade -y
+sudo apt install python3 python3-pip build-essential libssl-dev libffi-dev python3-dev -y
+/usr/bin/python3 /home/salazarylizanicole/assignmentnumber6/first.py
+/usr/bin/python3 /home/salazarylizanicole/assignmentnumber6/second.py
---
+++ assignmentnumber6/second (1).py
@@ -0,0 +1,143 @@
+#Program 2: Addition Quiz
+#Create a program that automatically generate two random numbers to add (0 to 99)
+#Let the user answer and evaluate if the user has the correct answer
+#The program will ask the user 10 addition operation
+#Display the result summary of the 10 operations (ex 9/10)
+
+import random
+import math
+def intro ():
+    print ("Hello User! this a addition quiz game")
+    name = input("what is your name?: ")
+    name = name.title()
+    print ("""Hello {}, welcome again to this program! enter your answer. Good Luck! """. format(name))
+    quest = input ("are you ready? yes or no: ")
+    if quest == "yes": 
+        print ("Let's play!")
+    else:
+        print ("that's okay, no pressure! i'll give the first number and there's no time limit!")
+
+
+def infos ():
+    score= 0
+    
+    number1 = random.randint(0,99)
+    number2 = random.randint(0,99)
+    total = number1 + number2
+    print(number1,"+",number2, end= "")
+    answer1 = int(input("="))
+    if answer1 == total : 
+        print("Correct!")
+        score = score +1
+    else: 
+        print("Incorrect :(")
+
+
+    number1 = random.randint(0,99)
+    number2 = random.randint(0,99)
+    total = number1+number2
+    print(number1,"+",number2, end= "")
+    answer1 = int(input("="))
+    if answer1 == total : 
+        print("Correct!")
+        score = score +1
+    else: 
+        print("Incorrect :(")
+    
+    number1 = random.randint(0,99)
+    number2 = random.randint(0,99)
+    total = number1+number2
+    print(number1,"+",number2, end= "")
+    answer1 = int(input("="))
+    if answer1 == total : 
+        print("Correct!")
+        score = score +1
+    else: 
+        print("Incorrect :(")
+
+    number1 = random.randint(0,99)
+    number2 = random.randint(0,99)
+    total = number1+number2
+    print(number1,"+",number2, end= "")
+    answer1 = int(input("="))
+    if answer1 == total : 
+        print("Correct!")
+        score = score +1
+    else: 
+        print("Incorrect :(")
+
+    number1 = random.randint(0,99)
+    number2 = random.randint(0,99)
+    total = number1+number2
+    print(number1,"+",number2, end= "")
+    answer1 = int(input("="))
+    if answer1 == total : 
+        score = score +1
+        print("Correct!")
+    else: 
+        print("Incorrect :(")
+    
+    number1 = random.randint(0,99)
+    number2 = random.randint(0,99)
+    total = number1 + number2
+    print(number1,"+",number2, end= "")
+    answer1 = int(input("="))
+    if answer1 == total : 
+        print("Correct!")
+        score = score +1
+    else: 
+        print("Incorrect :(")
+
+
+    number1 = random.randint(0,99)
+    number2 = random.randint(0,99)
+    total = number1+number2
+    print(number1,"+",number2, end= "")
+    answer1 = int(input("="))
+    if answer1 == total : 
+        print("Correct!")
+        score = score +1
+    else: 
+        print("Incorrect :(")
+    
+    number1 = random.randint(0,99)
+    number2 = random.randint(0,99)
+    total = number1+number2
+    print(number1,"+",number2, end= "")
+    answer1 = int(input("="))
+    if answer1 == total : 
+        print("Correct!")
+        score = score +1
+    else: 
+        print("Incorrect :(")
+
+    number1 = random.randint(0,99)
+    number2 = random.randint(0,99)
+    total = number1+number2
+    print(number1,"+",number2, end= "")
+    answer1 = int(input("="))
+    if answer1 == total : 
+        print("Correct!")
+        score = score +1
+    else: 
+        print("Incorrect :(")
+
+    number1 = random.randint(0,99)
+    number2 = random.randint(0,99)
+    total = number1+number2
+    print(number1,"+",number2, end= "")
+    answer1 = int(input("="))
+    if answer1 == total : 
+        score = score +1
+        print("Correct!")
+    else: 
+        print("Incorrect :(")
+
+    print("Your total score is " + str(score) + " out of 10")
+    
+def goodbye ():
+    print ("thank you for playing!")
+
+intro ()
+infos ()
+goodbye ()
